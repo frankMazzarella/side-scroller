6383c244d86c5e60bee17caca524eb92c090f2a1
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-09-08 17:56:32 -0400

    adding physics and movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Movement: MonoBehaviour {
+    public Rigidbody rigidbody;
+
+	void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(0, 3500, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = rigidbody.transform.position;
+            position.x += 3.5f * Time.deltaTime;
+            rigidbody.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = rigidbody.transform.position;
+            position.x -= 3f * Time.deltaTime;
+            rigidbody.transform.position = position;
+        }
+    }
+}
