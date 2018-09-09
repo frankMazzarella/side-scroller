0213c8aae21e137dbdced8d985a416af0e152dc9
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-09-08 17:41:58 -0700

    adding double jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,26 +1,71 @@
 ﻿using UnityEngine;
 
-public class Movement: MonoBehaviour {
-    public Rigidbody rigidbody;
+public class Movement : MonoBehaviour
+{
+    public Rigidbody player;
+    private int jumpForce = 3500;
+    private int doubleJumpForce = 4500;
+    private float moveSpeed = 3.5f;
+    private bool allowJump = true;
+    private bool allowDoubleJump = false;
 
-	void Update() {
+    private void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbody.AddForce(0, 3500, 0);
+            handleJump();
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Vector3 position = rigidbody.transform.position;
-            position.x += 3.5f * Time.deltaTime;
-            rigidbody.transform.position = position;
+            moveRight();
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Vector3 position = rigidbody.transform.position;
-            position.x -= 3f * Time.deltaTime;
-            rigidbody.transform.position = position;
+            moveLeft();
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Ground")
+        {
+            allowJump = true;
+            allowDoubleJump = false;
+        }
+    }
+
+    private void handleJump()
+    {
+        if (allowDoubleJump)
+        {
+            Debug.Log("double jump");
+            allowJump = false;
+            allowDoubleJump = false;
+            player.AddForce(0, doubleJumpForce, 0);
+        }
+
+        if (allowJump)
+        {
+            Debug.Log("single jump");
+            allowJump = false;
+            allowDoubleJump = true;
+            player.AddForce(0, jumpForce, 0);
+        }
+    }
+
+    private void moveRight()
+    {
+        Vector3 position = player.transform.position;
+        position.x += moveSpeed * Time.deltaTime;
+        player.transform.position = position;
+    }
+
+    private void moveLeft()
+    {
+        Vector3 position = player.transform.position;
+        position.x -= moveSpeed * Time.deltaTime;
+        player.transform.position = position;
     }
 }
